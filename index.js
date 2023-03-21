36d7da9af89978005b06cd34cc2c38286fd4c081
Selena Williams <s.a.williams242@gmail.com>
2023-03-21 16:45:29 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers (drivers) {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers (drivers) {
+    return drivers.splice(2, 3);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (integer) {
+    return function (fare) {
+        return fare * integer;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn) {
+    if (fn === returnFirstTwoDrivers) {
+      return drivers.slice(0, 2);
+    } else if (fn === returnLastTwoDrivers) {
+      return drivers.slice(-2);
+    }
+  }
+  
+  function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
